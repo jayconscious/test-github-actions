b4c78dd3ac2a1e24d3dd7f1f7e05970fe6c386dc
jayconscious <zhuzhiyong_wade@163.com>
2021-08-17 17:03:10 +0800

    chore: update deploy.yml

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   title: 'Hello Vuepress Docker test',
   description: 'Just playing around',
-  base: 'https://unpkg.com/zzy-test-github-actions@latest/docs/.vuepress/dist',
+  base: '/',
 }
